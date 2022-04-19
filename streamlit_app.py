d4ba93a234be965cd14b416f4d0ce0a8cf282975
Jacqueline Liao <jfliao@andrew.cmu.edu>
2022-04-19 10:47:11 -0400

    Add data files and initial streamlit layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,16 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+
+st.title("On the morning of January 28, 2022, Pittsburgh's Forbes Avenue Bridge collapsed just hours before President Joe Biden's visit to the city to promote his infrastructure plan.")
+st.title("This led us to wonder, what is the current status of Pennsylvania's bridges?")
+
+st.header("Let's first take a look at how Pennsylvania compares to the rest of the country.")
+
+st.header("Let's have a high-level overview on the condition of PA bridges.")
+
+st.header("What types of bridges need the most help?")
+
+st.header("How much funding do we need to fix and repair these bridges?")
+
 
